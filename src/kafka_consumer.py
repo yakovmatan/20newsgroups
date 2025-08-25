874b7aa58dd474c649d958b40d72fbf4b96301b8
yakov matan <yakovmatan@gmail.com>
2025-08-25 17:20:28 +0300

    create data

--- src/kafka_consumer.py
+++ src/kafka_consumer.py
@@ -1,1 +1,21 @@
+from kafka_configurations import Configurations
 
+class Consumer:
+
+    def __init__(self, topic):
+        self.event = Configurations().get_consumer_events(topic)
+
+    def consumer_with_auto_commit(self):
+        self.print_messages(self.event)
+
+
+    def print_messages(self,events):
+        for message in events:
+            print("%s:%d:%d: key=%s value=%s" % (message.topic, message.partition,
+                                                 message.offset, message.key,
+                                                 message.value))
+
+
+if __name__ == '__main__':
+     e = Consumer("topic1")
+     e.consumer_with_auto_commit()
--- src/producer.py
+++ src/producer.py
@@ -1,6 +1,5 @@
 from kafka_configurations import Configurations
 
-
 class Producer:
 
     def __init__(self):
@@ -9,25 +8,8 @@ class Producer:
 
     def publish_message(self,topic,message):
         self.producer.send(topic, message)
-
+        self.producer.flush()
 
     def publish_message_with_key(self,topic,key,message):
         self.producer.send(topic, key=key, value=message)
-
-    def flush(self):
         self.producer.flush()
-
-
-if __name__ == '__main__':
-    producer = Producer()
-    event = {"App":"Producer 1"}
-    event_1 = {"App": "Producer 2"}
-
-    #Publish message to a topic
-    producer.publish_message("topic1",event)
-
-    #Publish message to a topic with key to enable hashed partitioning
-    producer.publish_message_with_key("topic1",b"client1",event_1)
-
-    # block until all async messages are sent
-    producer.flush()
