61a8f0f80dad5ff7ae1f81741e63f4a51a6002f1
yakov matan <yakovmatan@gmail.com>
2025-08-25 23:22:28 +0300

    set consumer massag interesting

--- src/consumer/consumer_massage_interesting.py
+++ src/consumer/consumer_massage_interesting.py
@@ -1,16 +1,27 @@
+from src.MongoDB.mongodb_connection import DbConnection
 from src.kafka_configurations import Configurations
+from datetime import datetime, timezone
 
 class ConsumerMassageInteresting:
 
     def __init__(self, topic='interesting'):
         self.event = Configurations().get_consumer_events(topic)
+        self.connection = DbConnection()
+        self.collection = self.connection.db[topic]
 
     def consumer_with_auto_commit(self):
-        self.print_messages(self.event)
+        self.insert_to_mongodb(self.event)
 
 
-    def print_messages(self,events):
+    def insert_to_mongodb(self,events):
         for message in events:
-            print("%s:%d:%d: key=%s value=%s" % (message.topic, message.partition,
-                                                 message.offset, message.key,
-                                                 message.value))
+            try:
+                data = message.value  # כבר dict בגלל value_deserializer
+
+                # הוספת חותמת זמן
+                data["created_at"] = datetime.now(timezone.utc)
+
+                self.collection.insert_one(data)
+                return f"Message inserted: {data}"
+            except Exception as e:
+                return f"❌ Failed to insert message: {e}"
